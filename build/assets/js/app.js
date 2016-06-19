a25c744d7fc481d13b5e7f5645336be388147e97
Vladimir Slepkov <slepkov@academ.org>
2016-06-19 13:31:51 +0600

    commit for the meeting

---
+++ build/assets/js/app.js
@@ -0,0 +1,12 @@
+(function() {
+  'use strict';
+
+  setTimeout(function() {
+    document.querySelector('.greating_picture').classList.add('m--show');
+  }, 1000);
+})();
+
+$(document).ready(function() {
+	console.log('я на главной странице');
+});
+//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIihmdW5jdGlvbigpIHtcclxuICAndXNlIHN0cmljdCc7XHJcblxyXG4gIHNldFRpbWVvdXQoZnVuY3Rpb24oKSB7XHJcbiAgICBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcuZ3JlYXRpbmdfcGljdHVyZScpLmNsYXNzTGlzdC5hZGQoJ20tLXNob3cnKTtcclxuICB9LCAxMDAwKTtcclxufSkoKTtcclxuXHJcbiQoZG9jdW1lbnQpLnJlYWR5KGZ1bmN0aW9uKCkge1xyXG5cdGNvbnNvbGUubG9nKCfRjyDQvdCwINCz0LvQsNCy0L3QvtC5INGB0YLRgNCw0L3QuNGG0LUnKTtcclxufSk7Il0sInNvdXJjZVJvb3QiOiIvc291cmNlLyJ9
